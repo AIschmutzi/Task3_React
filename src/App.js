5d7068a0806bffc275254299b913ff23d921a9f0
itsaradh <145046994+itsaradh@users.noreply.github.com>
2023-10-14 19:58:41 +0530

    third commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Colorchoose from './Components/Colorchoose';
+
+
+function App() {
+  return (
+    <div class="background d-flex flex-column justify-content-center align-items-center">
+      <h1 class="text-center h1 fw-bold">ColorChoose </h1>
+    <Colorchoose/>
+    </div>
+
+  );
+}
+export default App;
